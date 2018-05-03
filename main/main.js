eacc63b368870233013d9fa39a202953081fb478
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-05-03 10:09:38 +0800

    出租车计价完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,15 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+'use strict';
+function getTaxiFee(distance, wait){
+    var taxiFee = 0;
+    if(distance <= 2){
+        taxiFee = 6;
+    }else if(distance > 2 && distance <= 8){
+        taxiFee = 6 + (distance - 2) * 0.8;
+    }else{
+        taxiFee = 10.8 + (distance - 8) * 1.2;
+    }
+    taxiFee = taxiFee + wait * 0.25;
+    return Math.round(taxiFee);
+}
+
+module.exports = getTaxiFee;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,26 @@
-const main = require('../main/main');
+//const main = require('../main/main');
+'use strict';
+var getTaxiFee = require('../main/main');
 
 describe('taxi fee', function () {
+    var taxiFee1 = 6;
+    var taxiFee2 = 9;
+    var taxiFee3 = 15;
+
+    it('两公里以内价格', function(){
+        var expectFee = getTaxiFee(2, 0);
+        expect(expectFee).toEqual(taxiFee1);
+    });
+
+    it('2公里到8公里价格', function(){
+        var expectFee = getTaxiFee(6, 1);
+        expect(expectFee).toEqual(taxiFee2);
+    })
+
+    it('8公里以上价格', function(){
+        var expectFee = getTaxiFee(9, 10);
+        expect(expectFee).toEqual(taxiFee3);
+    })
+    
     // write your tests here...
 });
